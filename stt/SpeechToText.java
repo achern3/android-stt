76db00ae36c878bee491bb599b5cd29bd1823bf2
Alan Chern <alan.chern@bluefletch.com>
2016-10-17 00:30:11 -0400

    Add SpeechToText file

---
+++ stt/SpeechToText.java
@@ -0,0 +1,187 @@
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by alanchern on 10/16/16.
+ * Speech-to-Text (STT) using Google APIs.
+ */
+
+public class SpeechToText {
+
+    public static final String WORD = RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH;
+    public static final String SENTENCE = RecognizerIntent.LANGUAGE_MODEL_FREE_FORM;
+
+    private static final String TAG = "SpeechToText";
+
+    private Context mContext;
+    private SpeechRecognizer mSpeechRecognizer;
+    private Intent mRecognizerIntent;
+    private boolean mEnableLogging = false;
+    private String mRecognitionResult;
+
+    /**
+     * @param context current context
+     * @param languageModel desired recognition language model, must be either
+     *                      RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH or RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+     * @exception InvalidLanguageModelException
+     */
+    public SpeechToText(Context context, String languageModel) {
+        mContext = context;
+
+        mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(mContext);
+        mSpeechRecognizer.setRecognitionListener(new Listener());
+
+        mRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        if (languageModel.equals(WORD)) {
+            mRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, WORD);
+        } else if (languageModel.equals(SENTENCE)) {
+            mRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, SENTENCE);
+        } else {
+            throw (new InvalidLanguageModelException("Invalid language model"));
+        }
+
+        mRecognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, context.getPackageName());
+    }
+
+    /**
+     * Initiate speech recognition.
+     */
+    public void recognize() {
+        mSpeechRecognizer.startListening(mRecognizerIntent);
+    }
+
+    /**
+     * Retrieve recognition result after running recognize(). This method should not be run in the main thread.
+     * @return String - the recognition result string
+     */
+    public String getResult() {
+        long start = System.currentTimeMillis();
+        while (mRecognitionResult == null && (System.currentTimeMillis() - start) / 1000 < 5) {
+            // wait until either recognition result is available or if 5 seconds have elapsed
+        }
+        String result = mRecognitionResult;
+        mRecognitionResult = null;
+        return result;
+    }
+
+    /**
+     * Enable speech recognizer progress logging information.
+     */
+    public void setProgressLogging(boolean enableLogging) {
+        mEnableLogging = enableLogging;
+    }
+
+    private class Listener implements RecognitionListener {
+
+        @Override
+        public void onReadyForSpeech(Bundle bundle) {
+            if (mEnableLogging) {
+                Log.d(TAG, "onReadyForSpeech");
+            }
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+            if (mEnableLogging) {
+                Log.d(TAG, "onBeginningOfSpeech");
+            }
+        }
+
+        @Override
+        public void onRmsChanged(float v) {
+            // Log.d(TAG, "onRmsChanged");
+        }
+
+        @Override
+        public void onBufferReceived(byte[] bytes) {
+            if (mEnableLogging) {
+                Log.d(TAG, "onBufferReceived");
+            }
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            if (mEnableLogging) {
+                Log.d(TAG, "onEndofSpeech");
+            }
+        }
+
+        @Override
+        public void onError(int error) {
+            if (mEnableLogging) {
+                Log.d(TAG, "onError: " + error);
+            }
+
+            // handle recognition error
+            switch (error) {
+                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                    Toast.makeText(mContext, "Network timeout error", Toast.LENGTH_SHORT).show();
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK:
+                    Toast.makeText(mContext, "Network error", Toast.LENGTH_SHORT).show();
+                    break;
+                case SpeechRecognizer.ERROR_AUDIO:
+                    Toast.makeText(mContext, "Audio error", Toast.LENGTH_SHORT).show();
+                    break;
+                case SpeechRecognizer.ERROR_SERVER:
+                    Toast.makeText(mContext, "Server error", Toast.LENGTH_SHORT).show();
+                    break;
+                case SpeechRecognizer.ERROR_CLIENT:
+                    Toast.makeText(mContext, "Client error", Toast.LENGTH_SHORT).show();
+                    break;
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                    Toast.makeText(mContext, "No speech input", Toast.LENGTH_SHORT).show();
+                    break;
+                case SpeechRecognizer.ERROR_NO_MATCH:
+                    Toast.makeText(mContext, "No match found", Toast.LENGTH_SHORT).show();
+                    break;
+                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                    Toast.makeText(mContext, "Recognizer busy error", Toast.LENGTH_SHORT).show();
+                    break;
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                    Toast.makeText(mContext, "Insufficient permissions error", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    Toast.makeText(mContext, "Error", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+
+            mSpeechRecognizer.cancel();
+        }
+
+        @Override
+        public void onResults(Bundle bundle) {
+            if (mEnableLogging) {
+                Log.d(TAG, "onResults");
+            }
+
+            // retrieve recognition result
+            mRecognitionResult = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION).get(0);
+        }
+
+        @Override
+        public void onPartialResults(Bundle bundle) {
+            // not useful
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle bundle) {
+            if (mEnableLogging) {
+                Log.d(TAG, "onEvent: " + eventType);
+            }
+        }
+    }
+
+    private class InvalidLanguageModelException extends RuntimeException {
+        private InvalidLanguageModelException(String message) {
+            super(message);
+        }
+    }
+}
